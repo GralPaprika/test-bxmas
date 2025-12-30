115a892172e4963bf8ea3f17d8d270d0ebd9bac6
CarlosFR4 <cfr4123@gmail.com>
2025-12-30 10:44:03 -0600

    refactor: remove unused import in PhotoDetailUiState

--- app/src/main/java/space/carlosrdgz/test/vepormas/ui/screens/photodetail/PhotoDetailUiState.kt
+++ app/src/main/java/space/carlosrdgz/test/vepormas/ui/screens/photodetail/PhotoDetailUiState.kt
@@ -1,7 +1,5 @@
 package space.carlosrdgz.test.vepormas.ui.screens.photodetail
 
-import space.carlosrdgz.test.vepormas.ui.screens.photodetail.PhotoDetailedInfo
-
 sealed class PhotoDetailUiState {
     data object Loading : PhotoDetailUiState()
     data class Success(val photo: PhotoDetailedInfo) : PhotoDetailUiState()
